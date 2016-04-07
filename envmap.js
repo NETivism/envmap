f004b0df6105da40fca92caca0daad452d0fc29a
hoshiliu <hoshi@netivism.com.tw>
2016-04-07 12:28:15 +0800

    add map to site.

--- envmap.js
+++ envmap.js
@@ -13,16 +13,34 @@ jQuery(document).ready(function($){
 
    $("#block-envmap-mapform").envmap({
       "control": "#edit-submit",
+      "twCounty": "/envmap/data/twCounty2010.json",
       "factory": "/sites/default/files/factory/finerealtime.js",
       "factoryPopupCallback": factoryDetail,
-      "airQuality": "/sites/default/files/airq/realtime.json",
+      "airquality": "/sites/default/files/airq/realtime.json",
       "toggleFactory": "#edit-factory-distbution",
-      "toggleAirq": "#edit-qualitystation"
+      "toggleAirquality": "#edit-qualitystation"
    });
 
 
+//factory & pollution option
    $('select#edit-factory-type').append($('select#edit-industry-name option').clone());
    $('select#edit-factory-poltype').append($('select#edit-poltype option').clone());
+
+
+//factory select
+   $("#edit-factory-distbution").change(function() {
+      $("#edit-factory-fine, #edit-factory-realtime, #edit-factory-overhead").prop("checked", false);
+   });
+
+//loading icon
+   $("head").append('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">');
+   $("#envmap-form .submit-button").append('<i class="fa fa-spinner fa-spin"></i>');
+   $(".fa-spinner").hide();
+   $("#envmap-form .submit-button").click(function() {
+      $(".fa-spinner").show();
+      setTimeout(function() { $(".fa-spinner").hide(); }, 1000);
+   });
+
 
 
 });
