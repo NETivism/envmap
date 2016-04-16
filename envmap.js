9bf900b5bf04a5d035294e68ed68df063e272cc9
hoshiliu <hoshi@netivism.com.tw>
2016-04-16 23:31:04 +0800

    update map popup

--- envmap.js
+++ envmap.js
@@ -1,11 +1,49 @@
 jQuery(document).ready(function($){
   var factoryDetail = function(e, factory){
     var popup = e.target.getPopup();
-    jQuery.getJSON('http://thaubing.gcaa.org.tw/json/factory/' + factory[0], function(popupJson){
+
+    var data = jQuery.getJSON('http://thaubing.gcaa.org.tw/json/factory/' + factory[0], function(popupJson){
       var popupText = '';
-      for (var index in popupJson["factory"][0]) {
-        popupText += index + ':' + popupJson["factory"][0][index] + '<br>';
+//      for (var index in popupJson["factory"][0]) {
+//	var ar = ["registration_no", "facility_name", "penalty_date","penalty_money"];
+//	if(jQuery.inArray(index, ar) != -1){
+//           popupText += index + ':' + popupJson["factory"][0][index] + '<br>';}
+//      }
+
+      //紀錄筆數
+      var cnt = 0;
+      for (var record in popupJson["factory"]){
+        cnt++;
       }
+      console.log(cnt);
+
+      //工廠名
+      popupText += '<div class="factory">' + popupJson["factory"][0]["facility_name"] + '</div>';
+
+      //列管類別
+      for (var index in popupJson["factory"][0]) {
+         var type = ["is_air", "is_water", "is_waste", "is_toxic"];
+         if(jQuery.inArray(index, type) != -1){
+            if(popupJson["factory"][0][index] == 1){
+	       popupText += '<div class="' + index + '">' + index + '</div>';
+            }
+         }
+      }
+
+      //所屬公司
+      popupText += '<div class="owner">所屬公司：' + popupJson["factory"][0]["corp_name"] + '</div>';
+
+      //最近開罰紀錄
+      popupText += '<div class="recent">最近一次開罰紀錄：' + popupJson["factory"][cnt-1]["penalty_date"] + ' - ' + popupJson["factory"][cnt-1]["penalty_money"] + '元</div>';
+
+      //開罰總額
+      var money = 0;
+      for(var i = 0; i < cnt; i++){
+         money += parseInt(popupJson["factory"][i]["penalty_money"]);
+      }
+      popupText += '<div class="statement">共被開罰</div><div class="focus">' + cnt + '</div><div class="statement">次，' + '合計</div><div class="focus">' + money + '</div><div class="statement">元</div>';
+ 
+
       popup.setContent(popupText);
       popup.update();
     });
